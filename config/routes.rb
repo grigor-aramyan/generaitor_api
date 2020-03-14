1b50f370581e2cf761b790cbc49f7f229da26b7a
grigor-aramyan <grigori.aramyan@gmail.com>
2020-03-14 11:11:13 +0400

    set up initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations, :sessions], controllers: { sessions: "sessions", registrations: "registrations" }
+  as :user do
+    post 'users' => 'registrations#create', :as => 'user_registration'
+
+    post 'users/sign_in' => 'devise/sessions#create', :as => 'user_session'
+    delete 'users/sign_out' => 'devise/sessions#destroy', :as => 'destroy_user_session'
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
