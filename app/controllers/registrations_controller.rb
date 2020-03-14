2debbb32c1ff15e57e446242c6f4f7aa192198bc
grigor-aramyan <grigori.aramyan@gmail.com>
2020-03-14 12:26:39 +0400

    add user models to schema

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
             if params[:user][:accountable_type] == 'Organization'
                 u = Organization.create(organization_sign_up_params(params[:user]))
             elsif params[:user][:accountable_type] == 'IdeaGeneraitor'
-                u = IdeaGeneraitor.create(idea_generator_sign_up_params(params[:user]))
+                u = IdeaGeneraitor.create(idea_generaitor_sign_up_params(params[:user]))
             end
 
             params[:user][:accountable_id] = u.id
@@ -16,12 +16,12 @@ class RegistrationsController < Devise::RegistrationsController
             params.require(:user).permit(:email, :password, :password_confirmation, :accountable_type, :accountable_id, :jti)
         end
 
-        def organization_sign_up_params
-            params.require(:organization).permit(:name, :logo_uri, :description)
+        def organization_sign_up_params(organization_params)
+            organization_params.require(:organization).permit(:name, :logo_uri, :description)
         end
 
-        def idea_generator_sign_up_params
-            params.require(:idea_generator).permit(:full_name, :avatar_uri, :description)
+        def idea_generaitor_sign_up_params(idea_generaitor_params)
+            idea_generaitor_params.require(:idea_generaitor).permit(:full_name, :avatar_uri, :description)
         end
 
   end
