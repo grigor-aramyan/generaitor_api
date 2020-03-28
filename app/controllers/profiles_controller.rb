a22654f4203181924a9429b9636b4fcf938f3a04
grigor-aramyan <grigori.aramyan@gmail.com>
2020-03-28 20:59:29 +0400

    make profiles show action work w/o auth

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,35 +1,47 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def show
-    profile = current_user.accountable
+    
+    current_user = nil
+    if User.exists?(params[:id])
+      current_user = User.find(params[:id])
+    end
 
-    profile_data = nil
-    if current_user.accountable_type == 'Organization'
-      profile_data = {
-        profile_id: profile.id,
-        name: profile.name,
-        logo_uri: profile.logo_uri,
-        description: profile.description
-      }
-    elsif current_user.accountable_type == 'IdeaGeneraitor'
-      profile_data = {
-        profile_id: profile.id,
-        full_name: profile.full_name,
-        avatar_uri: profile.avatar_uri,
-        description: profile.description
+    if current_user == nil
+      render status: :bad_request
+    else  
+      profile = current_user.accountable
+
+      profile_data = nil
+      if current_user.accountable_type == 'Organization'
+        profile_data = {
+          profile_id: profile.id,
+          name: profile.name,
+          logo_uri: profile.logo_uri,
+          description: profile.description
+        }
+      elsif current_user.accountable_type == 'IdeaGeneraitor'
+        profile_data = {
+          profile_id: profile.id,
+          full_name: profile.full_name,
+          avatar_uri: profile.avatar_uri,
+          description: profile.description
+        }
+      end
+
+      render json: {
+        data: {
+          user: {
+            user_id: current_user.id,
+            email: current_user.email,
+            profile_type: current_user.accountable_type
+          },
+          profile: profile_data
+        }
       }
     end
 
-    render json: {
-      data: {
-        user: {
-          user_id: current_user.id,
-          email: current_user.email,
-          profile_type: current_user.accountable_type
-        },
-        profile: profile_data
-      }
-    }
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     post 'users/sign_in' => 'devise/sessions#create', :as => 'user_session'
     delete 'users/sign_out' => 'sessions#destroy', :as => 'destroy_user_session'
 
-    get 'profiles/show'
+    get 'profiles/show/:id', to: 'profiles#show'
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
